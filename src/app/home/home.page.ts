858485f2eb3710ff1c24871ddc4fde71f48d3116
RichmondCg <RichmondGillaco.neust@gmail.com>
2024-12-07 18:09:35 +0800

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public title: string = 'Portfolio';
+  public profileA: string = 'Chlyde Adrian E. Benavidez';
+  public profileB: string = 'Johnray P. Cabatic';
+  public mukha: string = 'mukha';
+  public paa: string = '© Late na Portfolio nina Onak at Eydi';
+
+  isProfileA: boolean = true;
+
+ 
+  profilePicture: string = '../../assets/eydi.jpg'; 
+
   constructor() {}
 
+  
+  toggleProfile(switchProfile: any) {
+    this.isProfileA = switchProfile.detail.checked;
+    this.profilePicture = this.isProfileA
+      ? '../../assets/eydi.jpg' 
+      : '../../assets/kano.jpg'; 
+  }
 }
